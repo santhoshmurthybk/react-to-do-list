bd46ebbae23516ad3b2c4cc0ad04de360d11b102
unknown <santhoshmurthybk@gmail.com>
2020-06-26 21:43:34 +0530

    Updates

---
+++ precache-manifest.af17432a357406b703b707a0c61e0659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697cecedd8e476f91fba8f3556bb3678",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "07a07f944b933e4d467f",
+    "url": "/react-to-do-list/static/css/main.a664ef0b.chunk.css"
+  },
+  {
+    "revision": "65aa4e6a943927ad6b8b",
+    "url": "/react-to-do-list/static/js/2.9e7991e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do-list/static/js/2.9e7991e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07a07f944b933e4d467f",
+    "url": "/react-to-do-list/static/js/main.93ca1468.chunk.js"
+  },
+  {
+    "revision": "9a7311025e51a361b9b1",
+    "url": "/react-to-do-list/static/js/runtime-main.5cb2f8e5.js"
+  }
+]);
